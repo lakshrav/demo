1360776a3129b6ccd5e5b4eb7810ba81d12b60e1
lakshrav <lakshravi23@gmail.com>
2024-05-25 19:14:46 +0530

    added ui

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:image_picker/image_picker.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:spell_check_on_client/spell_check_on_client.dart';
 import 'package:flutter/services.dart';
-
+import 'package:path_drawing/path_drawing.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,30 +15,54 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Text Recognition App',
+ 
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        textTheme: TextTheme(
+          bodyText1: TextStyle(fontFamily: 'Gilroy'),
+          bodyText2: TextStyle(fontFamily: 'Lato'),
+        ),
       ),
       home: MyHomePage(),
     );
   }
 }
 
-
-
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Text Recognition App'),
+        backgroundColor: Colors.white, // Set the AppBar background color to green
       ),
+      backgroundColor: Colors.white, // Set the background color to green
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
-              onPressed: () async {
+            Padding(
+              padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.1),
+              child: Text(
+                'NutriScan',
+                style: TextStyle(
+                  fontFamily: 'Gilroy',
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color:Color(0xFF0ea771), 
+                ),
+              ),
+            ),
+            SizedBox(height: 10), // Adjust the space between texts
+            Text(
+              'Check the quality of the products you are consuming.',
+              style: TextStyle(
+                fontFamily: 'Lato',
+                fontSize: 20, // Adjust font size as needed
+              ),
+              textAlign: TextAlign.center, // Center-align the text
+            ),
+            SizedBox(height: 20), // Space before the button
+            GestureDetector(
+              onTap: () async {
                 XFile? image = await ImagePicker().pickImage(
                   source: ImageSource.gallery,
                   imageQuality: 100, // adjust as required
@@ -49,14 +73,70 @@ class MyHomePage extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) =>
-                              DisplayImageScreen(croppedImage.path)),
+                        builder: (context) => DisplayImageScreen(croppedImage.path),
+                      ),
                     );
                   }
                 }
               },
-              child: Text('Upload Image'),
+
+              child: CustomPaint(
+                painter: DottedBorderPainter(borderColor: Color(0xFF0ea771), strokeWidth: 3, dashWidth: 5),
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[100], // Light grey fill
+
+                    borderRadius: BorderRadius.circular(30), // Increase the border radius for softer edges
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/images/upload_2.png',
+                        width: 30,
+                        height: 30,
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Upload Image',
+                        style: TextStyle(
+                          fontFamily: 'Lato',
+                          fontSize: 16,
+                          color: Colors.grey[800],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
+            SizedBox(height: 20), // Space before the button
+            ElevatedButton(
+  
+              onPressed: () async {
+                // Open Camera functionality
+              },
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(
+                  vertical: MediaQuery.of(context).size.height * 0.02,
+                  horizontal: MediaQuery.of(context).size.width * 0.1,
+                ),
+                backgroundColor: Color(0xFFe7faf4),
+                foregroundColor: Color(0xFF12d18e),
+                shadowColor: Colors.transparent,
+                side: BorderSide.none,
+              ),
+              child: Text(
+                'Open Camera',
+                style: TextStyle(
+                  fontFamily: 'Lato',
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.1), // Add space at the bottom
           ],
         ),
       ),
@@ -76,7 +156,7 @@ class MyHomePage extends StatelessWidget {
       uiSettings: [
         AndroidUiSettings(
           toolbarTitle: 'Crop Image',
-          toolbarColor: Colors.deepOrange,
+          toolbarColor: Color(0xFF0ea771),
           toolbarWidgetColor: Colors.white,
           initAspectRatio: CropAspectRatioPreset.original,
           lockAspectRatio: false,
@@ -94,6 +174,36 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
+class DottedBorderPainter extends CustomPainter {
+  final Color borderColor;
+  final double strokeWidth;
+  final double dashWidth;
+
+  DottedBorderPainter({required this.borderColor, this.strokeWidth = 1, this.dashWidth = 5});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint paint = Paint()
+      ..color = borderColor
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = strokeWidth;
+
+    final Path path = Path()
+      ..addRRect(RRect.fromRectAndRadius(
+          Rect.fromLTWH(0, 0, size.width, size.height), Radius.circular(30)));
+
+    final dashArray = CircularIntervalList<double>(<double>[dashWidth, dashWidth]);
+
+    canvas.drawPath(
+      dashPath(path, dashArray: dashArray),
+      paint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
+
 class DisplayImageScreen extends StatefulWidget {
   final String imagePath;
   DisplayImageScreen(this.imagePath);
@@ -107,13 +217,11 @@ class _DisplayImageScreenState extends State<DisplayImageScreen> {
   String recog_text = '...';
   SpellCheck? spellCheck;
 
-
   @override
   void initState() {
     super.initState();
     performOcr();
   }
- 
 
   Future<void> performOcr() async {
     final textRecognizer = TextRecognizer();
@@ -141,20 +249,34 @@ class _DisplayImageScreenState extends State<DisplayImageScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Display Image'),
+        backgroundColor: Color(0xFF0ea771),
+         iconTheme: IconThemeData(color: Colors.white), // Set the AppBar background color to green
+        title: Text('Display Ingredients', style: TextStyle(color: Colors.white,fontFamily: 'Lato')),
       ),
       body: SingleChildScrollView(
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // Image.file(File(widget.imagePath)),
-              // SizedBox(height: 20),
-              Text(recog_text, style: TextStyle(fontSize: MediaQuery.of(context).size.width * 0.05)),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(16.0),
+                child: SingleChildScrollView(
+                  child: Text(
+                    recog_text,
+                    style: TextStyle(
+                      fontFamily: 'Lato',
+                      fontSize: MediaQuery.of(context).size.width * 0.05,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
       ),
     );
   }
+
 }
